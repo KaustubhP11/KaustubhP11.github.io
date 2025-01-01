69d9305672844db9b4a6f7b9e3502c501b68c315
KaustubhP11 <62472334+KaustubhP11@users.noreply.github.com>
2025-01-01 15:08:01 +0000

    Deploying to gh-pages from @ KaustubhP11/KaustubhP11.github.io@916ce85c49a10b1b29d59a9d1cb057c434649eff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -410,12 +410,7 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-our-latest-work-lora-silver-bullet-is-out-on-arxiv",
+    },{id: "news-our-latest-work-lora-silver-bullet-is-out-on-arxiv",
           title: 'Our latest work LoRA-Silver Bullet is out on arxiv 😄',
           description: "",
           section: "News",},{id: "news-attending-neurips-2024-to-preswnt-our-work-on-federated-lora",
